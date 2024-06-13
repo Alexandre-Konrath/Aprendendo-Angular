6549e34a6c20f4f90a6b33a881f30197644fd4f3
Alexandre-Konrath <delimakonrath@gmail.com>
2024-06-13 08:26:34 -0300

    file with the routes to the main page, list page and filter the list by id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: 'list/:id', component: ItemDetailsComponent }
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
