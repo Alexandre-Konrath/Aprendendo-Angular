f325f62fff23a65587109a4ed0387b045e3211f3
Alexandre-Konrath <delimakonrath@gmail.com>
2024-06-14 09:14:31 -0300

    app.modules onde fica todos os imports da aplicação, como componente e bibliotecas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { ChageNumberComponent } from './components/chage-number/chage-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { SqrtPipe } from './pipe/sqrt.pipe';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { HomeComponent } from './components/home/home.component';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ParentDataComponent,
+    DirectivesComponent,
+    IfRenderComponent,
+    EventosComponent,
+    EmitterComponent,
+    ChageNumberComponent,
+    ListRenderComponent,
+    PipesComponent,
+    SqrtPipe,
+    TwoWayBindingComponent,
+    HomeComponent,
+    ItemDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
