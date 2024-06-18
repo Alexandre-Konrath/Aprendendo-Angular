145a9a0a1f920f12f841ebd5f2a1ee88bcb8edf1
Alexandre-Konrath <delimakonrath@gmail.com>
2024-06-18 07:10:55 -0300

    app.modules onde fica todos os imports da aplicação, como componente … …e bibliotecas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
+import { ChageNumberComponent } from './components/chage-number/chage-number.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
-import { ChageNumberComponent } from './components/chage-number/chage-number.component';
-import { ListRenderComponent } from './components/list-render/list-render.component';
 import { PipesComponent } from './components/pipes/pipes.component';
-import { SqrtPipe } from './pipe/sqrt.pipe';
-import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 import { HomeComponent } from './components/home/home.component';
-import { ItemDetailsComponent } from './components/item-details/item-details.component';
+import { SqrtPipe } from './pipe/sqrt.pipe';
 
 @NgModule({
   declarations: [
